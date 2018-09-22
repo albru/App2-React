81d946e30c65b8021f1db1c3b3edab06ede48303
Alexandr Bryukhanov <bryukhanovspb@gmail.com>
2018-09-22 19:30:43 +0300

    first commit

---
+++ webpack.config.js
@@ -0,0 +1,43 @@
+const HtmlWebPackPlugin = require("html-webpack-plugin");
+const devMode = process.env.NODE_ENV !== 'production';
+
+
+const htmlPlugin = new HtmlWebPackPlugin({
+  template: "./src/index.html",
+  filename: "./index.html"
+});
+
+module.exports = {
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+        }  
+      },
+      {
+        test: /\.(sa|sc|c)ss$/,
+        use: [
+          devMode ? 'style-loader' : 
+                "css-loader",
+                "postcss-loader",
+                "sass-loader"
+        ]
+      },
+      {
+        test: /\.(png|svg|jpg|gif)$/,
+        use: [
+         'file-loader'
+        ]
+      }
+    ]
+  },
+  devServer: {
+    publicPath: '/',
+    watchContentBase: true,
+    historyApiFallback: true
+  },
+  plugins: [htmlPlugin]
+};
