d909fc879535f328bd6be1309e6245177219d8d9
Alexandr Bryukhanov <bryukhanovspb@gmail.com>
2018-09-23 22:46:18 +0300

    prepare src files

--- src/App.js
+++ src/App.js
@@ -1,13 +1,8 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import Navbar from './components/Navbar'
-import Home from './components/Home'
-import Contact from './components/Contact'
-import About from './components/About'
-import { BrowserRouter, Route } from 'react-router-dom'
 
 class App extends React.Component {
-  constructor(props) {
+  constructor(props) { 
     super(props)
     this.state = {
       
@@ -15,19 +10,12 @@ class App extends React.Component {
   }
   render() {
     return (
-      <BrowserRouter>
-        <div className="App">
-          <Navbar />
-          <Route exact path='/' component={Home} />
-          <Route path='/about' component={About} />
-          <Route path='/contact' component={Contact} />
-        </div>
-      </BrowserRouter>
+      <div>
+        <h1>MarioPlan</h1>
+      </div>
     )
   }
 }
-
-ReactDOM.render(<App />, document.getElementById('app'))
 
 export default App
 
--- src/index.js
+++ src/index.js
@@ -2,8 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App"
 // import style from "./main.scss";
-
-
+ReactDOM.render(<App />, document.getElementById('app'))
 
 
 
